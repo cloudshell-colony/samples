ecf0c4aabe7d9f46a4b4896d8fa146623f5d2aeb
Meni Besso <meni.b@quali.com>
2018-09-06 13:46:13 +0300

    update

--- applications/rails-website/rails-website-hc.sh
+++ applications/rails-website/rails-website-hc.sh
@@ -4,14 +4,7 @@ ip=$1
 port=3000
 hostname=${ip}:${port}
 
-for (( ; ; ))
-do
-    sleep 1
-    if [[ $(wget -S -T1 -t1 http://${hostname}/swagger/ 2>&1 | grep "HTTP/" | awk '{print $2}') == "200" ]] ; then
-        echo 'connected to' ${hostname}
-        exit 0
-    else
-        echo 'cannot connect to' ${hostname}
-    fi
-done
-exit 1
+until timeout 1 bash -c "</dev/tcp/$ip/$port"; [[ "$?" -eq "0" ]];
+  do sleep 1;
+done;
+echo 'connected to' "$hostname"
