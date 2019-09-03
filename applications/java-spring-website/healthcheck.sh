b922998f98196d388e6d9dc81575bddceec9af51
asafc64 <asafc64@gmail.com>
2019-09-03 10:43:37 +0300

    Update healthcheck.sh

--- applications/java-spring-website/healthcheck.sh
+++ applications/java-spring-website/healthcheck.sh
@@ -8,10 +8,10 @@ for (( ; ; ))
 do
     sleep 1
     if [[ $(wget -S -T1 -t1 "http://${hostname}/" 2>&1 | grep "HTTP/1" | awk '{print $2}') == "200" ]] ; then
-        echo 'connected to' ${hostname}
+        echo "connected to ${hostname}"
         exit 0
     else
-        echo 'cannot connect to' ${hostname}
+        echo "cannot connect to ${hostname}"
     fi
 done
 exit 1
