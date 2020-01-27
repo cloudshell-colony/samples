805028a5de1e8e548d80e6ebd40191be2d9eddec
eyalperlm <56922923+eyalperlm@users.noreply.github.com>
2020-01-27 10:11:32 +0200

    Delete wordpress-start-k8s.sh

--- applications/wordpress/wordpress-start-k8s.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-echo "****************************************************************"
-echo "Restarting Apache"
-echo "****************************************************************"
-service apache2 restart
-while ps -p "$(pgrep apache2)" > /dev/null ; do sleep 1; done;
