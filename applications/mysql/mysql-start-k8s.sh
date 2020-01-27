65aefbb125f5e49f09a5ee72c2c38134412dac4f
eyalperlm <56922923+eyalperlm@users.noreply.github.com>
2020-01-27 10:11:12 +0200

    Delete mysql-start-k8s.sh

--- applications/mysql/mysql-start-k8s.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-echo "****************************************************************"
-echo "Restarting MYSQL"
-echo "****************************************************************"
-service mysql restart
-while ps -p "$(pgrep mysql)" > /dev/null ; do sleep 1; done;
